148e65ffd893f1fdda154e696e63eabc7d45a581
Sai Prajapati <167699795+saiprajapati@users.noreply.github.com>
2026-09-08 23:35:01 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d1 = {}
+        d2 = {}
+        if len(s) != len(t):
+            return False
+        for i in range (len(s)):
+            if(s[i]) not in d1:
+                d1[s[i]] = 1
+            else:
+                d1[s[i]] += 1
+
+            if (t[i]) not in d2:
+                d2[t[i]] = 1
+            else:
+                d2[t[i]] += 1
+        return d1 == d2
